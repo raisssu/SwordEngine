a5135a94858e4833028913aec112266c43dad741
raisssu <rizoan789@gmail.com>
2025-07-25 15:11:51 -0400

    library init and setup, window creation

--- SwordEngine/win32_main.cpp
+++ SwordEngine/win32_main.cpp
@@ -1,13 +1,60 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main() {
 
+    //glfw initialization
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); -- needed for Mac
 
+    //glfw window creation
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "WINDOW", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate(); //unable to create glfw window
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    //GLAD opengl functions
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1; //failed to initialize glad
+    }
+
+    //render loop
+    while (!glfwWindowShouldClose(window)) {
+
+        //input
+        processInput(window);
+
+        //render
+        glClearColor(191.f/255.f, 170.f/255.f, 170.f / 255.f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        //buffer check and swap
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
 }
