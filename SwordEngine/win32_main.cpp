3b0f7f4d302d119bcbd1064e8a617d16e641b442
raisssu <rizoan789@gmail.com>
2025-08-02 01:24:04 -0400

    feat: add use of element buffer object for rendering

--- SwordEngine/win32_main.cpp
+++ SwordEngine/win32_main.cpp
@@ -96,22 +96,36 @@ int main() {
 
     //vertex input
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.0f,  0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,  //top right
+         0.5f, -0.5f, 0.0f,  //bottom right
+        -0.5f,  0.5f, 0.0f,  //top left
+        -0.5f, -0.5f, 0.0f,  //bottom left
     };
-    unsigned int VBO, VAO;
+
+    unsigned int indices[] = {
+        0, 1, 2, // first triangle
+        2, 3, 1  //second
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    //wireframe 
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     //render loop
     while (!glfwWindowShouldClose(window)) {
@@ -126,7 +140,7 @@ int main() {
         //drawing object
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         //buffer check and swap
         glfwSwapBuffers(window);
